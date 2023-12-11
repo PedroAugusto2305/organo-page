6d7a294c2bd781c63f1248a87495655a64ed93bc
PedroAugusto2305 <p.augusto_23@outlook.com>
2023-12-11 10:38:35 -0300

    Functional form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import Banner from "./components/Banner/Banner.js";
 import Formulario from "./components/Formulario/index.js";
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+  };
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorAdicionado(colaborador)
+        }
+      />
     </div>
   );
 }
